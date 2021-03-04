c428b00ac3a6445c7c8143c9f8b5a4455cb4f217
Daniel151505 <daniel150520011@gmail.com>
2021-03-04 18:56:54 -0500

    Importando Router

--- Aplicacion-Preguntas-Respuestas/src/app/components/respuesta/respuesta.component.ts
+++ Aplicacion-Preguntas-Respuestas/src/app/components/respuesta/respuesta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Pregunta } from 'src/app/models/pregunta.module';
 import { PreguntaService } from 'src/app/services/pregunta.service';
 
@@ -10,8 +11,8 @@ import { PreguntaService } from 'src/app/services/pregunta.service';
 export class RespuestaComponent implements OnInit {
   listarPreguntas: Pregunta[]
   respuestasUsuario: any []
-  constructor(private preguntaService: PreguntaService) { }
-
+  constructor(private preguntaService: PreguntaService,
+              private router: Router) { }
   ngOnInit(): void {
     this.listarPreguntas = this.preguntaService.preguntas
     this.respuestasUsuario = this.preguntaService.respuestaUsuario
