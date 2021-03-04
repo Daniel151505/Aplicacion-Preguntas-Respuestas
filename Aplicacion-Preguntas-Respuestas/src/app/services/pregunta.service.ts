0020ea7f0268a1ed3bd9eac2515530601d6e4485
Daniel151505 <daniel150520011@gmail.com>
2021-03-04 00:58:50 -0500

    Creando Arreglo de Preguntas y Respuestas

--- Aplicacion-Preguntas-Respuestas/src/app/services/pregunta.service.ts
+++ Aplicacion-Preguntas-Respuestas/src/app/services/pregunta.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Pregunta } from '../models/pregunta.module';
+import { Respuesta } from '../models/respuesta.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntaService {
 
+  public preguntas: Pregunta[] = [
+    new Pregunta('Cual es la capital de Argentina', [
+      new Respuesta('Buenos Aires', 1),
+      new Respuesta('Montevideo', 0),
+      new Respuesta('Santiago', 0),
+      new Respuesta('Lima', 0),
+    ]),
+    new Pregunta('Cual es la capital de Francia', [
+      new Respuesta('Roma', 0),
+      new Respuesta('Paris', 1),
+      new Respuesta('Dublin', 0),
+      new Respuesta('Atenas', 0),
+    ]),
+    new Pregunta('Cual es la capital de Egipto', [
+      new Respuesta('Londres', 0),
+      new Respuesta('Berlin', 0),
+      new Respuesta('El Cairo', 1),
+      new Respuesta('Casablanca', 0),
+    ]),
+  ];
   constructor() { }
+
+
 }
